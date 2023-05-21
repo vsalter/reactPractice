9bb516aa53cb03318d792153273eb6e66d62824a
vsalter <vsalter1@student.gsu.edu>
2023-05-21 16:45:46 -0400

    imported Routes and Route into App.jsx. Defined routes for NewOrderPage and OrderHistoryPage

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,16 +1,21 @@
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import './App.css';
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
 
   return (
     <main className="App">
       { user ?
-        <NewOrderPage />
+        <Routes>
+          <Route path='/orders/new' element={<NewOrderPage />} />
+          <Route path='/orders' element={<OrderHistoryPage />} />
+        </Routes>
         :
         <AuthPage />
       }
