d041bbedabb5d88b098326d87e20b657c5c1b66e
vsalter <vsalter1@student.gsu.edu>
2023-05-21 15:03:20 -0400

    conditionally rendered neworderpage or authpage in App.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,13 +1,19 @@
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
   const [user, setUser] = useState(null);
-  
+
   return (
     <main className="App">
-      App
+      { user ?
+        <NewOrderPage />
+        :
+        <AuthPage />
+      }
     </main>
   );
 }
