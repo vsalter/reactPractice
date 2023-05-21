15f66368fe6517cbcee851a61f22a897389be8b4
vsalter <vsalter1@student.gsu.edu>
2023-05-21 17:34:15 -0400

    created navbar component and added it to App.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,6 +4,7 @@ import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import './App.css';
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
 
 function App() {
 
@@ -12,10 +13,13 @@ function App() {
   return (
     <main className="App">
       { user ?
+      <>
+       <NavBar/>
         <Routes>
           <Route path='/orders/new' element={<NewOrderPage />} />
           <Route path='/orders' element={<OrderHistoryPage />} />
         </Routes>
+      </>
         :
         <AuthPage />
       }
