ebb36c92aa9dffe29420cd871fcbcd71b3b1905e
vsalter <vsalter1@student.gsu.edu>
2023-06-06 14:39:43 -0400

    changed user state back to null and added SignUpForm component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -8,7 +8,7 @@ import NavBar from '../../components/NavBar/NavBar';
 
 function App() {
 
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   return (
     <main className="App">
