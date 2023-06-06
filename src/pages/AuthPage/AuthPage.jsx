2069efa64b56258f7b2b366e51bfca4dd756e4ec
vsalter <vsalter1@student.gsu.edu>
2023-06-06 14:42:54 -0400

    imported and rendered SignUpForm component within AuthPage component

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,11 +1,13 @@
 import './AuthPage.css';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
 
 function AuthPage() {
   
   return (
-    <h1>
-        AuthPage
-    </h1>
+    <main>
+      <h1>AuthPage</h1>
+      <SignUpForm />
+    </main>
   );
 }
 
